014a4d37ea3621ea65b090d25b52f6a4b9936e6d
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-10 17:01:40 -0600

    commit tree be4185462ca9847291a0793bf2239220f15b3756

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// // code your solution here
+// const record = [
+//     { year: "2015", result: "L"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "W"},
+// ]
+
+
+function superbowlWin (objArray) {
+
+    if (objArray.find(object => object.result === 'W')) {
+        return (objArray.find(object => object.result === 'W').year);
+    } else {
+        return undefined;
+    }
+}
+
+// superbowlWin(record);
